660840164b69562bf36d759a546d7ef098c6fcf0
Prathmesh Parab <93149250+prathmesh0@users.noreply.github.com>
2023-01-09 01:18:03 +0530

    Add files via upload

---
+++ A-Z/Sorting Techniques/insertion_sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void InsertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+int main()
+{
+    int arr[1000];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    InsertionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
