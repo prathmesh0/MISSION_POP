78c75b39ab45f123bd602b8d5669503af1bcee8b
Prathmesh Parab <93149250+prathmesh0@users.noreply.github.com>
2023-02-01 21:58:23 +0530

    Add files via upload

---
+++ A-Z/Problems On Array/Hard Level/largest_sunbarray_with0sum.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution
+{
+public:
+    int maxLen(vector<int> &A, int n)
+    {
+        int maxi = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int sum = 0;
+            for (int j = i; j < n; j++)
+            {
+                sum += A[j];
+                if (sum == 0)
+                {
+                    maxi = max(maxi, j - i + 1);
+                }
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int m;
+        cin >> m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i)
+        {
+            cin >> array1[i];
+        }
+        Solution ob;
+        cout << ob.maxLen(array1, m) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
