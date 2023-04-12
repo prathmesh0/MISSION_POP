bb4e680af3d9a988d27e86781c99317cbb0c7adb
Prathmesh Parab <93149250+prathmesh0@users.noreply.github.com>
2023-04-12 12:00:18 +0530

    Add files via upload

---
+++ A-Z/Linked List/Medium Problem Of Linked List/palindromeLLornot.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int num;
+    node *next;
+    node(int val)
+    {
+        num = val;
+        next = NULL;
+    }
+};
+
+void insertNode(node *head, int val)
+{
+    node *newNode = new node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return;
+}
+
+// brute force approach
+/*
+bool isPalindrome(node *head)
+{
+    vector<int> arr;
+    while (head != NULL)
+    {
+        arr.push_back(head->num);
+        head = head->next;
+    }
+    for (int i = 0; i < arr.size() / 2; i++)
+        if (arr[i] != arr[arr.size() - i - 1])
+            return false;
+    return true;
+}
+*/
+// optimal approach
+node *reverse(node *ptr)
+{
+    node *pre = NULL;
+    node *nex = NULL;
+    while (ptr != NULL)
+    {
+        nex = ptr->next;
+        ptr->next = pre;
+        pre = ptr;
+        ptr = nex;
+    }
+    return pre;
+}
+
+bool isPalindrome(node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return true;
+
+    node *slow = head;
+    node *fast = head;
+
+    while (fast->next != NULL && fast->next->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    slow->next = reverse(slow->next);
+    slow = slow->next;
+    node *dummy = head;
+
+    while (slow != NULL)
+    {
+        if (dummy->num != slow->num)
+            return false;
+        dummy = dummy->next;
+        slow = slow->next;
+    }
+    return true;
+}
+int main()
+{
+    node *head = NULL;
+    insertNode(head, 1);
+    insertNode(head, 2);
+    insertNode(head, 3);
+    insertNode(head, 2);
+    insertNode(head, 1);
+    isPalindrome(head) ? cout << "True" : cout << "False";
+    return 0;
+}
