e88fdd4b6effabc8b8cb9378d7c270024719c612
Prathmesh Parab <93149250+prathmesh0@users.noreply.github.com>
2023-07-20 23:58:51 +0530

    Add files via upload

---
+++ A-Z/Binary Search/BS on 1D Arrays/searchInRotatedSortedOne.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Bruteforce
+// int search(vector<int> &arr, int n, int k)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] == k)
+//             return i;
+//     }
+//     return -1;
+// }
+
+//Optimal
+int search(vector<int> &arr,int n, int target)
+{
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+
+        // left half sorted
+        if (arr[low] <= arr[mid])
+        {
+            if (arr[low] <= target && target <= arr[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        // right half sorted
+        else
+        {
+            if (arr[mid] <= target && target <= arr[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+
+    vector<int> arr = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+    int n = 9, k = 1;
+    int ans = search(arr, n, k);
+    if (ans == -1)
+        cout << "Target is not present.\n";
+    else
+        cout << "The index is: " << ans << "\n";
+    return 0;
+}
