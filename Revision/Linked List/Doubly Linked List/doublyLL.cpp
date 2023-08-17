bf74fe15f5d5009cddefe2bd8b6d29bdfaaf2c0a
Prathmesh Parab <93149250+prathmesh0@users.noreply.github.com>
2023-08-17 23:48:11 +0530

    Add files via upload

---
+++ Revision/Linked List/Doubly Linked List/doublyLL.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+
+    ~Node()
+    {
+        int val = this->data;
+        if (next != NULL)
+        {
+            delete next;
+            next = NULL;
+        }
+        cout << "Memory free for node with data " << val << endl;
+    }
+};
+
+// Insert At Head
+void insertAthead(Node *&head, Node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *temp = new Node(data);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        Node *temp = new Node(data);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *node1 = new Node(10);
+    // cout << node1->data << " ";
+    // cout << node1->next << " ";
+    // cout << node1->prev << " ";
+    Node *head = node1;
+    Node *tail = head;
+    insertAthead(head, tail, 15);
+    print(head);
+
+    return 0;
+}
