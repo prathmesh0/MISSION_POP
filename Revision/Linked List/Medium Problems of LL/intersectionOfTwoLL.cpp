7766203434bb27bcc40d1004002afb1e37baf844
Prathmesh Parab <93149250+prathmesh0@users.noreply.github.com>
2023-08-28 23:23:49 +0530

    Add files via upload

---
+++ Revision/Linked List/Medium Problems of LL/intersectionOfTwoLL.cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+        int num;
+        node* next;
+        node(int val) {
+            num = val;
+            next = NULL;
+        }
+};
+
+void insertNode(node* &head,int val) {
+    node* newNode = new node(val);
+    
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    
+    node* temp = head;
+    while(temp->next != NULL) temp = temp->next;
+    
+    temp->next = newNode;
+    return;
+}
+
+//Brute force Approach
+
+// node* intersectionPresent(node* head1,node* head2) {
+//     while(head2 != NULL) {
+//         node* temp = head1;
+//         while(temp != NULL) {
+//             //if both nodes are same
+//             if(temp == head2) return head2;
+//             temp = temp->next;
+//         }
+//         head2 = head2->next;
+//     }
+//     //intersection is not present between the lists return null
+//     return NULL;
+// }
+
+
+//Better Approach
+// node* intersectionPresent(node* head1,node* head2) {
+//      unordered_set<node*> st;
+//     while(head1 != NULL) {
+//        st.insert(head1);
+//        head1 = head1->next;
+//     }
+//     while(head2 != NULL) {
+//         if(st.find(head2) != st.end()) return head2;
+//         head2 = head2->next;
+//     }
+//     return NULL;
+
+// }
+
+//Optimal Approach
+int getDifference(node* head1,node* head2) {
+     int len1 = 0,len2 = 0;
+        while(head1 != NULL || head2 != NULL) {
+            if(head1 != NULL) {
+                ++len1; head1 = head1->next;
+            }
+            if(head2 != NULL) {
+                ++len2; head2 = head2->next;
+            }
+            
+        }
+        //if difference is neg-> length of list2 > length of list1 else vice-versa
+        return len1-len2;
+}
+
+
+node* intersectionPresent(node* head1,node* head2) {
+ int diff = getDifference(head1,head2);
+        if(diff < 0) 
+            while(diff++ != 0) head2 = head2->next; 
+        else while(diff-- != 0) head1 = head1->next;
+        while(head1 != NULL) {
+            if(head1 == head2) return head1;
+            head2 = head2->next;
+            head1 = head1->next;
+        }
+        return head1;
+
+
+}
+void printList(node* head) {
+    while(head->next != NULL) {
+        cout<<head->num<<"->";
+        head = head->next;
+    }
+    cout<<head->num<<endl;
+}
+
+int main() {
+    // creation of both lists 
+    node* head = NULL;
+    insertNode(head,1);
+    insertNode(head,3);
+    insertNode(head,1);
+    insertNode(head,2);
+    insertNode(head,4);
+    node* head1 = head;
+    head = head->next->next->next;
+    node* headSec = NULL;
+    insertNode(headSec,3);
+    node* head2 = headSec;
+    headSec->next = head;
+    //printing of the lists
+    cout<<"List1: "; printList(head1);
+    cout<<"List2: "; printList(head2);
+    //checking if intersection is present
+    node* answerNode = intersectionPresent(head1,head2);
+    if(answerNode == NULL )
+    cout<<"No intersection\n";
+    else
+    cout<<"The intersection point is "<<answerNode->num<<endl;
+    return 0;
+}
