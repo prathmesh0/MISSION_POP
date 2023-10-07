caf150d875504752f7a41e8736ebcb69139d0885
Prathmesh Parab <93149250+prathmesh0@users.noreply.github.com>
2023-10-07 23:33:47 +0530

    Add files via upload

---
+++ A-Z/Recursion Patternwise/Subsequences Pattern/subsequeceWithsumK.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void subsequence(vector<int> &v, int sum, vector<int> &ds, int ind, int s, int n)
+{
+    // BC
+    if (ind == n)
+    {
+        if (sum == s)
+        {
+            for (auto it : ds)
+                cout << it << " ";
+                cout<<endl;
+        }
+        return;
+    }
+
+    // Include
+    ds.push_back(v[ind]);
+    s += v[ind];
+    subsequence(v, sum, ds, ind + 1, s, n);
+    // Exclude
+    s -= v[ind];
+    ds.pop_back();
+    subsequence(v, sum, ds, ind + 1, s, n);
+}
+int main()
+{
+    vector<int> v = {1, 2, 1};
+    int sum = 2;
+    int n = 3;
+    vector<int> ds;
+    subsequence(v, sum, ds, 0, 0, n);
+    return 0;
+}
